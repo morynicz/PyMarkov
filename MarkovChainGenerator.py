223aaeb1c76cc1b2016bd5097b1f347ae9fd98aa
Michał Orynicz <morynicz@gmail.com>
2021-11-04 23:55:14 +0100

    Just before introduction of randomness

--- MarkovChainGenerator.py
+++ MarkovChainGenerator.py
@@ -3,11 +3,14 @@ class InvalidStatesException(Exception):
 
 
 def make_markov_chain_generator(state_table):
-    if (not isinstance(state_table, list)) or (not len(state_table) > 0):
+    if (not isinstance(state_table, list)) or (not len(state_table) > 0) or any(
+            sum(state_table[i]) != 1 for i in range(len(state_table))):
         raise InvalidStatesException
 
     def gen():
+        state = 0
         while True:
-            yield 0
+            yield state
+            state = state_table[state].index(1)
 
     return gen()
--- tests.py
+++ tests.py
@@ -20,7 +20,25 @@ class MarkovChainGeneratorTestSuite(unittest.TestCase):
         sut = make_markov_chain_generator([[1]])
         self.assertEqual([0, 0, 0, 0, 0], [next(sut) for i in range(5)])
 
+    def test_given_chain_with_two_alternating_states_when_generates_repeatedly_then_returns_0_1_0_1(self):
+        sut = make_markov_chain_generator([[0, 1], [1, 0]])
+        self.assertEqual([0, 1, 0, 1], [next(sut) for i in range(4)])
 
+    def test_given_chain_with_two_states_with_second_looping_when_generates_repeatedly_then_returns_0_1_1_1(self):
+        sut = make_markov_chain_generator([[0, 1], [0, 1]])
+        self.assertEqual([0, 1, 1, 1], [next(sut) for i in range(4)])
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_given_chain_with_three_circling_states_when_generates_repeatedly_then_returns_0_2_1_0_2(self):
+        sut = make_markov_chain_generator([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+        self.assertEqual([0, 2, 1, 0], [next(sut) for i in range(4)])
+
+    def test_given_state_transitions_not_summing_to_1_in_row_when_then_throws_invalid_states_exception(
+            self):
+        with self.assertRaises(InvalidStatesException):
+            make_markov_chain_generator([[0.1]])
+
+    def test_given_state_transitions_not_summing_to_1_in_different_row_then_throws_invalid_states_exception(
+            self):
+        with self.assertRaises(InvalidStatesException):
+            make_markov_chain_generator([[0, 1], [0.9, 0]])
+

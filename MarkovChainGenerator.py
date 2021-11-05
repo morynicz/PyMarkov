0d10da436898ec4f56de9b1229710ad9a079682c
Michał Orynicz <morynicz@gmail.com>
2021-11-05 14:42:55 +0100

    Added randomness handling

--- MarkovChainGenerator.py
+++ MarkovChainGenerator.py
@@ -1,16 +1,21 @@
+from itertools import accumulate
+
+
 class InvalidStatesException(Exception):
     pass
 
 
-def make_markov_chain_generator(state_table):
+def make_markov_chain_generator(state_table, rng):
     if (not isinstance(state_table, list)) or (not len(state_table) > 0) or any(
             sum(state_table[i]) != 1 for i in range(len(state_table))):
         raise InvalidStatesException
+
+    distributions = [[y for y in accumulate(x)] for x in state_table]
 
     def gen():
         state = 0
         while True:
             yield state
-            state = state_table[state].index(1)
-
+            roll = next(rng)
+            state = next(st for st, weight in enumerate(distributions[state]) if weight > roll)
     return gen()
--- tests.py
+++ tests.py
@@ -1,44 +1,52 @@
 import unittest
+from itertools import repeat
 
 from MarkovChainGenerator import InvalidStatesException, make_markov_chain_generator
 
 
 class MarkovChainGeneratorTestSuite(unittest.TestCase):
+    @staticmethod
+    def make_sut(state_table, rng=repeat(0.99)):
+        return make_markov_chain_generator(state_table, rng)
+
     def test_when_chain_without_state_traversal_matrix_creation_attempted_then_throw_invalid_states_exception(self):
         with self.assertRaises(InvalidStatesException):
-            make_markov_chain_generator(None)
+            self.make_sut(None)
 
     def test_when_chain_with_empty_state_traversal_matrix_creation_attempted_then_throw_invalid_states_exception(self):
         with self.assertRaises(InvalidStatesException):
-            make_markov_chain_generator([])
+            self.make_sut([])
 
     def test_given_chain_with_one_state_when_generates_then_returns_0(self):
-        sut = make_markov_chain_generator([[1]])
+        sut = self.make_sut([[1]])
         self.assertEqual(0, next(sut))
 
     def test_given_chain_with_one_state_when_generates_multiple_times_then_always_returns_0(self):
-        sut = make_markov_chain_generator([[1]])
+        sut = self.make_sut([[1]])
         self.assertEqual([0, 0, 0, 0, 0], [next(sut) for i in range(5)])
 
     def test_given_chain_with_two_alternating_states_when_generates_repeatedly_then_returns_0_1_0_1(self):
-        sut = make_markov_chain_generator([[0, 1], [1, 0]])
+        sut = self.make_sut([[0, 1], [1, 0]])
         self.assertEqual([0, 1, 0, 1], [next(sut) for i in range(4)])
 
     def test_given_chain_with_two_states_with_second_looping_when_generates_repeatedly_then_returns_0_1_1_1(self):
-        sut = make_markov_chain_generator([[0, 1], [0, 1]])
+        sut = self.make_sut([[0, 1], [0, 1]])
         self.assertEqual([0, 1, 1, 1], [next(sut) for i in range(4)])
 
     def test_given_chain_with_three_circling_states_when_generates_repeatedly_then_returns_0_2_1_0_2(self):
-        sut = make_markov_chain_generator([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
+        sut = self.make_sut([[0, 0, 1], [1, 0, 0], [0, 1, 0]])
         self.assertEqual([0, 2, 1, 0], [next(sut) for i in range(4)])
 
     def test_given_state_transitions_not_summing_to_1_in_row_when_then_throws_invalid_states_exception(
             self):
         with self.assertRaises(InvalidStatesException):
-            make_markov_chain_generator([[0.1]])
+            self.make_sut([[0.1]])
 
     def test_given_state_transitions_not_summing_to_1_in_different_row_then_throws_invalid_states_exception(
             self):
         with self.assertRaises(InvalidStatesException):
-            make_markov_chain_generator([[0, 1], [0.9, 0]])
+            self.make_sut([[0, 1], [0.9, 0]])
 
+    def test_given_probabilistic_state_transitions_and_rnd_generator_when_generates_then_returns_0_0_1_1(self):
+        sut = self.make_sut([[0.5, 0.5], [0, 1]], (x for x in [0.3, 0.6, 0, 0]))
+        self.assertEqual([0, 0, 1, 1], [next(sut) for i in range(4)])
